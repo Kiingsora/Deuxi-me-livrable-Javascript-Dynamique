a03a618466d9dd83bd198536d7acbe17197c8dc5
kiingsora <hichem.m83@gmail.com>
2026-04-08 13:07:54 +0200

    logique JS pour selection des boutons

--- FrontEnd/assets/category.js
+++ FrontEnd/assets/category.js
@@ -1,16 +1,36 @@
 const API_BASE_URL = await fetch('http://localhost:5678/api/categories');
 const reponse = await API_BASE_URL.json();
-console.log(reponse);
 
-const category = document.getElementById("category");
+const categorys = document.getElementById("category");
+let selectedButton = document.querySelector(".selected ");
 
-reponse.forEach(objet => {
+const allButton = document.createElement("button");
+allButton.innerText = "Tous";
+allButton.classList.add("btn-category", "selected"); // Sélectionné par défaut
+categorys.appendChild(allButton);
+
+allButton.addEventListener("click", () => {
+    document.querySelectorAll('.btn-category').forEach(btn => {
+        btn.classList.remove('selected');
+    });
     
-    const categorie = document.createElement("button");
-    categorie.innerText = objet.name;
-    categorie.classList = "btn-category"
-    category.appendChild(categorie);
-
+    allButton.classList.add('selected');
+    console.log("Affichage de tous les projets");
 });
 
-console.log(category);
+reponse.forEach(objet => {
+    const category = document.createElement("button");
+    category.innerText = objet.name;
+    category.classList.add("btn-category");
+
+    category.addEventListener("click", () => {
+        document.querySelectorAll('.btn-category').forEach(btn => {
+            btn.classList.remove('selected');
+        });
+
+        category.classList.add('selected');
+        console.log("Catégorie sélectionnée:", category.textContent);
+    });
+
+    categorys.appendChild(category);
+});
